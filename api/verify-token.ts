dfb2434937b5a81932aacd356d1fca17317886a8
ChristianP-7 <christianperezt7@gmail.com>
2024-10-13 15:42:59 -0500

    Probando conexión con eBay 6

--- api/verify-token.ts
+++ api/verify-token.ts
@@ -1,19 +1,26 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import crypto from 'crypto';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const verificationToken = process.env.VERIFICATION_TOKEN; // Token de verificación de eBay
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const verificationToken = process.env.VERIFICATION_TOKEN; // Token de eBay
+  const endpoint = process.env.ENDPOINT_URL; // URL completa del endpoint
 
   if (req.method === 'GET') {
-    const queryToken = req.query['verification-token'];
+    const challengeCode = req.query.challenge_code as string;
 
-    if (queryToken === verificationToken) {
-      return res.status(200).send(queryToken); // Devuelve el token si es correcto
-    } else {
-      return res
-        .status(400)
-        .json({ message: 'Token de verificación inválido' });
+    if (!challengeCode || !verificationToken || !endpoint) {
+      return res.status(400).json({ message: 'Faltan parámetros' });
     }
+
+    // Crea el hash con SHA-256
+    const hash = crypto.createHash('sha256');
+    hash.update(challengeCode);
+    hash.update(verificationToken);
+    const result = hash.digest(endpoint);
+
+    // Devuelve el hash como respuesta
+    return res.status(200).send(result.toString('hex'));
   } else {
-    res.status(405).json({ message: 'Método no permitido' });
+    return res.status(405).json({ message: 'Método no permitido' });
   }
 }
