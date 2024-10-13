dcded5d3a2e239d3b064e5f1261aecf2df547b37
ChristianP-7 <christianperezt7@gmail.com>
2024-10-13 15:53:34 -0500

    Probando conexión con eBay 8

--- api/verify-token.ts
+++ api/verify-token.ts
@@ -1,8 +1,8 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import crypto from 'crypto';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const verificationToken = process.env.VERIFICATION_TOKEN; // Token de eBay
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const verificationToken = process.env.VERIFICATION_TOKEN; // Token de verificación de eBay
   const endpoint = process.env.ENDPOINT_URL || ''; // URL completa del endpoint
 
   if (req.method === 'GET') {
@@ -12,16 +12,24 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
       return res.status(400).json({ message: 'Faltan parámetros' });
     }
 
-    // Crea el hash con SHA-256
-    const hash = crypto.createHash('sha256');
-    hash.update(challengeCode);
-    hash.update(verificationToken);
-    hash.update(Buffer.from(endpoint, 'utf-8')); // Convertir endpoint a Buffer
+    try {
+      // Crea el hash con SHA-256
+      const hash = crypto.createHash('sha256');
+      hash.update(challengeCode);
+      hash.update(verificationToken);
+      hash.update(Buffer.from(endpoint, 'utf-8')); // Convertir endpoint a Buffer
 
-    const result = hash.digest('hex'); // Devuelve el hash en formato hexadecimal
+      // Obtiene el hash y lo convierte en formato hexadecimal
+      const result = hash.digest('hex');
 
-    // Devuelve el hash como respuesta
-    return res.status(200).send(result);
+      // Devuelve el hash como respuesta
+      return res.status(200).send(result);
+    } catch (error) {
+      console.error('Error al generar el hash:', error);
+      return res
+        .status(500)
+        .json({ message: 'Error al procesar la solicitud' });
+    }
   } else {
     return res.status(405).json({ message: 'Método no permitido' });
   }
